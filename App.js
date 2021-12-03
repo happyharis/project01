b93f72336da85d5597914e9e5400ec5b0038ee43
Haris Samingan <muhamad_haris@hotmail.com>
2021-12-03 10:11:32 +0800

    Add icons

--- App.js
+++ App.js
@@ -4,16 +4,38 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "./screens/HomeScreen";
 import ContactScreen from "./screens/ContactScreen";
 import EventsScreen from "./screens/EventsScreen";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarActiveTintColor: "tomato",
+          tabBarInactiveTintColor: "gray",
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === "Home") {
+              iconName = focused ? "home" : "home-outline";
+            } else if (route.name === "Contact") {
+              iconName = focused ? "happy" : "happy-outline";
+            } else if (route.name === "Events") {
+              iconName = focused ? "calendar" : "calendar-outline";
+            }
+
+            // You can return any component that you like here!
+            // return <FontAwesome name={iconName} size={size} color={color} />;
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+      >
         <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Events" component={EventsScreen} />
         <Tab.Screen name="Contact" component={ContactScreen} />
+        <Tab.Screen name="Events" component={EventsScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
